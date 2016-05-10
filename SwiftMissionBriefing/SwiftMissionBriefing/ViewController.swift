0afe23015359f16f6a272b1e5ac16eb1c021c721
Donny Davis <donnywdavis@icloud.com>
2016-05-10 13:36:01 -0400

    Link up the button action

--- SwiftMissionBriefing/SwiftMissionBriefing/ViewController.swift
+++ SwiftMissionBriefing/SwiftMissionBriefing/ViewController.swift
@@ -28,7 +28,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: Button Actions
+    
+    @IBAction func authenticateAction(sender: UIButton) {
+        
+    }
 
 }
 
